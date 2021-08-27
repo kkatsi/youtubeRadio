394c1ae2a7bc8ac6f52c31e3b272eec7bfb785d3
unknown <kotsoulis@gmail.com>
2021-08-27 14:26:15 +0300

    styling changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./SCSS/App.scss";
 import axios from "axios";
 import ReactPlayer from "react-player";
 import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
-import { GiPocketRadio } from "react-icons/gi";
+import { GiPocketRadio, GiSettingsKnobs } from "react-icons/gi";
 import { AiOutlineLoading } from "react-icons/ai";
+import { MdVolumeUp, MdVolumeOff } from "react-icons/md";
+import Grid from "@material-ui/core/Grid";
+import Slider from "@material-ui/core/Slider";
 
 function App() {
+  let photoUrl, photoWidth, photoHeight;
   const [url, setUrl] = useState("");
   const [play, setPlay] = useState(false);
   const [data, setData] = useState();
@@ -17,13 +21,24 @@ function App() {
     </div>
   );
 
-  useEffect(() => {
-    document.querySelector(".content").style.height =
-      document.querySelector(".App").clientHeight -
-      20 -
-      document.querySelector("header").clientHeight +
-      "px";
-  }, []);
+  const [countryName, setCountryName] = useState("GR");
+  const [songName, setSongName] = useState("");
+  // const [songPhoto, setSongPhoto] = useState("");
+
+  const [volumeValue, setVolumeValue] = useState(100);
+  const [prevVolumeValue, setPrevVolumeValue] = useState();
+
+  const handleChange = (event, newValue) => {
+    setVolumeValue(newValue);
+  };
+
+  // useEffect(() => {
+  //   document.querySelector(".content").style.height =
+  //     document.querySelector(".App").clientHeight -
+  //     20 -
+  //     document.querySelector("header").clientHeight +
+  //     "px";
+  // }, []);
 
   function requestData() {
     axios({
@@ -46,6 +61,24 @@ function App() {
         .map(({ value }) => value);
       res.data.items = shuffled;
       setUrl(`https://www.youtube.com/watch?v=${res.data.items[0].id}`);
+      console.log(res.data.items[0]);
+      setSongName(res.data.items[0].snippet.localized.title);
+      if (window.innerWidth > 786) {
+        photoUrl = res.data.items[0].snippet.thumbnails.high.url;
+        photoWidth = res.data.items[0].snippet.thumbnails.high.width;
+        photoHeight = res.data.items[0].snippet.thumbnails.high.height;
+      } else {
+        photoUrl = res.data.items[0].snippet.thumbnails.medium.url;
+        photoWidth = res.data.items[0].snippet.thumbnails.medium.width;
+        photoHeight = res.data.items[0].snippet.thumbnails.medium.height;
+      }
+      document.querySelector(
+        ".songPhoto"
+      ).style.backgroundImage = `url(${photoUrl})`;
+      document.querySelector(".songPhoto").style.width = photoWidth + "px";
+      document.querySelector(".songPhoto").style.height = photoHeight + "px";
+      // document.querySelector(".App").style.backgroundColor = "transparent";
+
       setPlay(true);
       setIndex(0);
       setData(res.data);
@@ -61,6 +94,7 @@ function App() {
   function handlePlay() {
     if (!play) {
       if (!data) {
+        document.querySelector(".playButton").classList.add("move");
         setPlayText(
           <div className="loading">
             <AiOutlineLoading />
@@ -79,16 +113,77 @@ function App() {
     setIndex((prevIndex) => prevIndex + 1);
     if (index < data.items.length - 1) {
       setUrl(`https://www.youtube.com/watch?v=${data.items[index + 1].id}`);
-      console.log(data.items[index].id);
+      setSongName(data.items[index + 1].snippet.localized.title);
+      if (window.innerWidth > 786) {
+        photoUrl = data.items[index + 1].snippet.thumbnails.high.url;
+        photoWidth = data.items[index + 1].snippet.thumbnails.high.width;
+        photoHeight = data.items[index + 1].snippet.thumbnails.high.height;
+      } else {
+        photoUrl = data.items[index + 1].snippet.thumbnails.medium.url;
+        photoWidth = data.items[index + 1].snippet.thumbnails.medium.width;
+        photoHeight = data.items[index + 1].snippet.thumbnails.medium.height;
+      }
+      document.querySelector(
+        ".songPhoto"
+      ).style.backgroundImage = `url(${photoUrl})`;
+      document.querySelector(".songPhoto").style.width = photoWidth + "px";
+      document.querySelector(".songPhoto").style.height = photoHeight + "px";
+      // console.log(data.items[index].id);
     }
+  }
+
+  function mute(e) {
+    if (volumeValue === 0) {
+      setVolumeValue(prevVolumeValue);
+      return;
+    }
+    setPrevVolumeValue(volumeValue);
+    setVolumeValue(0);
   }
 
   return (
     <div className="App">
+      {/* <div
+        className="bg-image"
+        style={{ backgroundImage: `url(${songPhoto})` }}
+      ></div>
+      <div className="overlay"></div> */}
       <header>
         YT Radio <GiPocketRadio />
       </header>
+
       <div className="content">
+        <div className="songPhoto"></div>
+        <div className="volumeSettings">
+          <span onClick={mute}>
+            {volumeValue !== 0 ? <MdVolumeUp /> : <MdVolumeOff />}
+          </span>
+          <Grid item xs>
+            <Slider
+              value={volumeValue}
+              onChange={handleChange}
+              aria-labelledby="continuous-slider"
+              style={{ marginLeft: ".5rem" }}
+            />
+          </Grid>
+        </div>
+        <span className="songName">
+          {songName !== "" ? (
+            <>
+              <span className="playingNow">Playing Now: </span>
+              <span>{songName}</span>
+            </>
+          ) : (
+            ""
+          )}
+        </span>
+        <div className="countrySettings">
+          <span>{countryName}</span>
+          <span>
+            <GiSettingsKnobs />
+          </span>
+        </div>
+
         <button className="playButton" onClick={handlePlay}>
           {playText}
         </button>
@@ -96,6 +191,8 @@ function App() {
 
       <ReactPlayer
         style={{ display: "none" }}
+        volume={volumeValue / 100}
+        muted={false}
         url={url}
         playing={play}
         onPlay={() => {
